af1afd39410ba512a08db4ab7c7054c88236d3b8
LockingReal <1191515088@qq.com>
2022-11-06 18:19:24 +0800

    refresh

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "first-meet" : "",
+  base: "first-meet",
   integrations: [solidJs(), tailwind()]
 });
